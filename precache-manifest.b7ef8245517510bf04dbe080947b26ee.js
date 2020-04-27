f367f2d4e6e258ee921178ba73ae3f331bba1e81
Diego Armando Portocarrero Espirilla <despearux@gmail.com>
2020-04-27 00:08:14 -0500

    Updates

---
+++ precache-manifest.b7ef8245517510bf04dbe080947b26ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb6d9451749f40dc1aa96e31bfbb8f52",
+    "url": "/react-monsters/index.html"
+  },
+  {
+    "revision": "8b92a517cf1932bf4b73",
+    "url": "/react-monsters/static/css/main.085af0ca.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/react-monsters/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b92a517cf1932bf4b73",
+    "url": "/react-monsters/static/js/main.a9b0b6d4.chunk.js"
+  },
+  {
+    "revision": "964fafda6df6ea44474f",
+    "url": "/react-monsters/static/js/runtime-main.1ebdd781.js"
+  }
+]);
